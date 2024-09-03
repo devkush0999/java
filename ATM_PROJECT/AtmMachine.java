c62e5ec0483ea6aa2222c9c9dd2b8c592e8d3c0f
devesh0999 <devesh2cse@gmail.com>
2024-09-03 20:59:24 +0530

    ATM project with the help of function and object

--- ATM_PROJECT/AtmMachine.java
+++ ATM_PROJECT/AtmMachine.java
@@ -18,6 +18,7 @@ class ATM {
         }
     }
 
+
     public void menu() {
         System.out.println("Enter Your Choice : ");
         System.out.println("1. Check Balance");
@@ -78,7 +79,7 @@ class ATM {
 
     public class AtmMachine {
         public static void main(String[] args) {
-            System.out.println("Welcome");
+            System.out.println("================================ Welcome to ATM bank ===================");
             ATM obj = new ATM();
             obj.checkpin();
 
